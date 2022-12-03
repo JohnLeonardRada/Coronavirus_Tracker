e835b224f79bcd0d7c1bde3f810fd9ca1a561243
John Leonard Rada <johnleonard.rada@gmail.com>
2022-12-03 19:58:50 +0800

    Initial Commit, Added Backend, Added Home And Total Cases Frontend

---
+++ src/main/java/com/project/coronavirustracker/controller/HomeController.java
@@ -0,0 +1,44 @@
+package com.project.coronavirustracker.controller;
+
+import com.project.coronavirustracker.model.LocationStats;
+import com.project.coronavirustracker.services.CoronavirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronavirusDataService coronavirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<LocationStats> allStats = coronavirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPreviousDay()).sum();
+
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+
+    @GetMapping("/total-cases.html")
+    public String totalCases(Model model){
+        List<LocationStats> allStats = coronavirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPreviousDay()).sum();
+
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "total-cases";
+    }
+
+}
---
+++ src/main/java/com/project/coronavirustracker/services/CoronavirusDataService.java
@@ -0,0 +1,64 @@
+package com.project.coronavirustracker.services;
+
+import com.project.coronavirustracker.model.LocationStats;
+import jakarta.annotation.PostConstruct;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CoronavirusDataService {
+
+    private static final String CONFIRMED_CASES_GLOBALLY_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+    private static final String RECOVERED_CASES_GLOBALLY_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
+    private static final String DEATH_CASES_GLOBALLY_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
+
+    private List<LocationStats> allStats = new ArrayList<>();
+
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
+    //After Done Creating The Instance Of The Class, Execute The Method With @PostConstruct
+    //Spring Supports Multiple @PostConstruct
+    @PostConstruct
+    //AUTO RUN BASED ON SET SCHEDULE
+    //@Scheduled(cron = "* * 1 * * *")
+    public void fetchVirusDate() throws IOException, InterruptedException {
+        List<LocationStats> newStats = new ArrayList<>();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(CONFIRMED_CASES_GLOBALLY_URL))
+                .build();
+
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            int latestCases = Integer.parseInt(record.get(record.size() - 1));
+            int previousDayCases = Integer.parseInt(record.get(record.size() - 2));
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPreviousDay(latestCases - previousDayCases);
+            System.out.println(locationStat);
+
+            newStats.add(locationStat);
+        }
+        this.allStats = newStats;
+    }
+
+
+}
